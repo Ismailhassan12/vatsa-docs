7e95132cbe03a5467966328e9b4569647c6db0aa
Ismailhassan12 <Zxcv221332@gmail.com>
2024-03-15 07:55:25 +0300

    Config

--- docs/OEJN.md
+++ docs/OEJN.md
@@ -1,12 +1,3 @@
----
-sidebar_label: 'Hi!'
-sidebar_position: 3
----
-
-# Hello
-
-This is my **first Docusaurus document**!
-
 # OEJN
 
 This is my **first Docusaurus document**!
--- docusaurus.config.js
+++ docusaurus.config.js
@@ -8,20 +8,20 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
+  title: 'Saudi Arabian vACC SOPs',
   tagline: 'Dinosaurs are cool',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://sops.vatsimsa.com',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/<vatsimsa-sop>/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'VATSIM Saudi Arabia', // Usually your GitHub org/user name.
+  projectName: 'vatsimsa-sop', // Usually your repo name.
 
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
