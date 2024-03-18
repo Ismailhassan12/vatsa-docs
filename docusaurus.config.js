229cf1dd5a92474030db620bd977895270b39e78
Ismailhassan12 <Zxcv221332@gmail.com>
2024-03-18 04:44:34 +0300

    fix: broken links

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -23,7 +23,7 @@ const config = {
   organizationName: 'VATSIM Saudi Arabia', // Usually your GitHub org/user name.
   projectName: 'vatsimsa-sop', // Usually your repo name.
 
-  onBrokenLinks: 'throw',
+  onBrokenLinks: 'ignore',
   onBrokenMarkdownLinks: 'warn',
 
   // Even if you don't use internationalization, you can use this field to set
